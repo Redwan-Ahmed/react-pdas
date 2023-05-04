f616144de2465a19898ec98311e60a8df0ff243a
Redwan <redwanahmed1@hotmail.co.uk>
2023-05-04 18:34:39 +0100

    completed cards added css lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImg from "./images/alexa.png";
+import SiriImg from "./images/siri.png";
+import CortanaImg from "./images/cortana.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexaAmazon" img={AlexaImg} desc={'Alexa was made by Amazon :)'}/>
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siriApple" img={SiriImg} desc={'Hey Siri? This was made by Apple.'}/>
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortanaMS"
+                img={CortanaImg}
+                desc={'Cortana was made by Microsoft.'}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
